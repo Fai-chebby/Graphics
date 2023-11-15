8c9fca1d7216c6d7fad40860311622cb9134d302
Fai-chebby <148770385+Fai-chebby@users.noreply.github.com>
2023-11-15 13:46:13 +0300

    😁

---
+++ triangle/triangle.cpp
@@ -0,0 +1,42 @@
+#include <GL/glut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    
+    glBegin(GL_TRIANGLES);
+	// Vertex 1: orange
+    glColor3f(1.0, 0.5, 0.0);
+    glVertex2f(0.0, 0.5);
+    
+    // Vertex 2: pink
+    glColor3f(1.0, 0.5, 0.8);
+    glVertex2f(-0.5, -0.5);
+
+    // Vertex 3: purple
+    glColor3f(0.5, 0.0, 0.5);
+    glVertex2f(0.5, -0.5);
+
+	glEnd();
+
+    glFlush();
+}
+
+void reshape(int w, int h) {
+    glViewport(0, 0, w, h);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("OpenGL Triangle Example");
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glClearColor(0.0, 0.0, 0.0, 0.0);  // Set the background color to black
+
+    glutMainLoop();
+    return 0;
+}
